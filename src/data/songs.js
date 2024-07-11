d0d1543a28e6d4ad5336eb4382a567fee1452e57
AkashdipGhosh <akashdipghosh1@gmail.com>
2024-07-11 20:24:15 +0530

    1st commit

---
+++ src/data/songs.js
@@ -0,0 +1,57 @@
+const songs = [
+  {
+      id: 1,
+      title: "Badri ka dulhaniya",
+      artist: "Dev Negi, Neha Kakkar, Monali Thakur, Ikka",
+      src: "/songs/song1.mp3",
+      cover: "/covers/cover1.jpg",
+  },
+  {
+      id: 2,
+      title: "Breakup Song",
+      artist: "Arijit Singh",
+      src: "/songs/song2.mp3",
+      cover: "/covers/cover2.jpg",
+  },
+  {
+    id: 3,
+    title: "Bekhayali",
+    artist: "Arijit Singh",
+    src: "/songs/song3.mp3",
+    cover: "/covers/cover3.jpg",
+  },{
+    id: 4,
+    title: "Wajha Tum Ho",
+    artist: "Armaan Malik, Tulsi Kumar",
+    src: "/songs/song4.mp3",
+    cover: "/covers/cover4.jpg",
+},
+{
+    id: 5,
+    title: "Dil Mein Chuhupa Loonga",
+    artist: "Armaan Malik, Tulsi Kumar",
+    src: "/songs/song5.mp3",
+    cover: "/covers/cover5.jpg",
+},
+{
+  id: 6,
+  title: "Dhadak-Title Song",
+  artist: "Shreya Ghosal, Ajay Atul",
+  src: "/songs/song6.mp3",
+  cover: "/covers/cover6.jpg",
+},{
+  id: 7,
+  title: "Bol Do Na Zara",
+  artist: "Armaan Malik",
+  src: "/songs/song7.mp3",
+  cover: "/covers/cover7.jpg",
+},{
+  id: 8,
+  title: "Chahun Main Ya Naa",
+  artist: "APalak Muchhal, Arijit Singh",
+  src: "/songs/song8.mp3",
+  cover: "/covers/cover8.jpg",
+}
+];
+
+export default songs;
